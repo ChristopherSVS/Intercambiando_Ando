546246b89032136c688f914a3b2218575ef64df4
ChristopherSVS <201440019@ucc.mx>
2022-05-21 11:36:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/intercambiando_ando/NuevaSesionActivity.java
@@ -0,0 +1,14 @@
+package com.example.intercambiando_ando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevaSesionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_sesion);
+    }
+}
